2437be8306d119ecf51b1830b1a1e0d476660c31
Tacite243 <taciteiragi@gmail.com>
2024-06-09 21:28:54 +0100

    add animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,30 @@
 
-
-
-let description = document.querySelectorAll('.description')
-description.closeBtn.addEventListener('hover', description.style.color = 'red');
+function animation() {
+    const galerys = document.querySelectorAll(".galery > div");
+  
+    galerys.forEach((galery) => {
+      const img = galery.querySelector("img");
+      const imageInfo = galery.querySelector(".description");
+      const link = galery.querySelector("a");
+  
+      // Add event listeners for hover effect
+      galery.addEventListener("mouseenter", function () {
+        imageInfo.style.top = "calc(100vh - 140px)";
+        img.style.transform = "scale(1.1)";
+        img.style.filter = "blur(6px) grayscale(100%)";
+        img.style.opacity = "0.5";
+        link.style.visibility = "unset";
+      });
+  
+      galery.addEventListener("mouseleave", function () {
+        imageInfo.style.top = "calc(100vh - 100px)";
+        img.style.transform = "scale(1)";
+        img.style.filter = "none";
+        img.style.opacity = "1";
+        link.style.visibility = "hidden";
+      });
+    });
+  }
+  
+  // Call the function to listen all the time
+  animation();
