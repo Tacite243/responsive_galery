9a1a52ac7f0109027d597c6c7bcfcd1f562965f5
Tacite243 <taciteiragi@gmail.com>
2024-06-09 15:31:55 +0100

    creating basic card

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const photos = document.querySelectorAll('.photo');
+
+//     photos.forEach(photo => {
+//         photo.addEventListener('mouseover', function() {
+//             const overlay = this.querySelector('.overlay');
+//             overlay.style.opacity = '1';
+//         });
+
+//         photo.addEventListener('mouseout', function() {
+//             const overlay = this.querySelector('.overlay');
+//             overlay.style.opacity = '0';
+//         });
+//     });
+// });
